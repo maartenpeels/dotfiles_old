86de5057f74ffe659f0ff0c84c148f96b0d1abb4
Maarten Peels <maartenpeels@gmail.com>
2024-07-03 11:52:43 +0200

    Changes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,27 +1,38 @@
+local builtin = require("telescope.builtin")
+
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = function()
-    local grep_from_git_root = function()
-      local root = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
-      if vim.v.shell_error == 0 then
-        require("telescope.builtin").live_grep({ cwd = root })
-      else
-        require("telescope.builtin").live_grep({ root = true })
-      end
-    end
-
-    local find_files_from_git_root = function()
-      local root = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
-      if vim.v.shell_error == 0 then
-        require("telescope.builtin").find_files({ cwd = root })
-      else
-        require("telescope.builtin").find_files({ root = true })
-      end
-    end
-
-    return {
-      { "<leader>/", grep_from_git_root, desc = "Grep (Root Dir)" },
-      { "<leader><space>", find_files_from_git_root, desc = "Find Files (Root Dir)" },
-    }
-  end,
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>/",
+        function()
+          local root = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+          if vim.v.shell_error == 0 then
+            builtin.live_grep({ cwd = root })
+          else
+            builtin.live_grep({ root = true })
+          end
+        end,
+        desc = "Grep (Git Dir)",
+      },
+      {
+        "<leader><space>",
+        function()
+          local root = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+          if vim.v.shell_error == 0 then
+            builtin.find_files({ cwd = root })
+          else
+            builtin.find_files({ root = true })
+          end
+        end,
+        desc = "Find File (Git Dir)",
+      },
+      {
+        "<leader>;",
+        builtin.resume,
+        desc = "Resume Telescope",
+      },
+    },
+  },
 }
